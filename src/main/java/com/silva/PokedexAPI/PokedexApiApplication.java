7f03854421de723814edd967ae8483789da5ec28
lucas <lucasssilva021@gmail.com>
2021-05-04 00:18:22 -0300

    Initial commit

---
+++ src/main/java/com/silva/PokedexAPI/PokedexApiApplication.java
@@ -0,0 +1,13 @@
+package com.silva.PokedexAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/silva/PokedexAPI/model/Pokemon.java
@@ -0,0 +1,41 @@
+package com.silva.PokedexAPI.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Pokemon {
+    int codigo;
+    String id, name, urlImage;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+}
